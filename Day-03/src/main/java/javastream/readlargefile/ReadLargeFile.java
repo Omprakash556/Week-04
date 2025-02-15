14d3e8ee1c7b66fbd0007365c841082ffacb1687
Om Prakash <ompandit416@gmail.com>
2025-02-15 12:19:43 +0530

    [OmPrakash]Add.

---
+++ Day-03/src/main/java/javastream/readlargefile/ReadLargeFile.java
@@ -0,0 +1,31 @@
+package javastream.readlargefile;
+
+
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadLargeFile {
+        public static void readErrorLines(String filePath) {
+            try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    if (line.toLowerCase().contains("error")) { // Case-insensitive check
+                        System.out.println(line);
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Error reading file: " + e.getMessage());
+            }
+        }
+
+    }
+     class LargeFileMain {
+        public static void main(String[] args) {
+            String filePath = "source.txt";
+            ReadLargeFile.readErrorLines(filePath);
+}
+
+    }
