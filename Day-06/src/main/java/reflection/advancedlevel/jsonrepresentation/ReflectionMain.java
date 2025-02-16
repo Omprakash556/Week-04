eb6b939f94ba44971707e195dc920a21405b0a59
Om Prakash <ompandit416@gmail.com>
2025-02-16 23:04:04 +0530

    [OmPrakash]Add.Reflection_Program_of_Day-06

--- Day-06/src/main/java/reflection/advancedlevel/jsonrepresentation/ReflectionMain.java
+++ Day-06/src/main/java/reflection/advancedlevel/jsonrepresentation/ReflectionMain.java
@@ -3,7 +3,7 @@ package reflection.advancedlevel.jsonrepresentation;
 public class ReflectionMain {
     public static void main(String[] args) {
         // Create an instance of the Person class
-        Person person = new Person("John Doe", 30, 5.9);
+        Person person = new Person("Om Prakash", 23, 5.3);
 
         // Generate JSON-like string using the JsonGenerator
         String jsonString = JsonGenerator.toJson(person);
